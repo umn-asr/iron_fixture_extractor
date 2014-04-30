3270365f12958791074d505a30784ab6460682f2
Christian Billen <cbillen@warmlyyours.com>
2014-04-30 11:43:42 -0500

    Prevents stack level too deep on inverse_of relations
    When a relation is specified with an inverse_of the association_cache
    will loop continuously between the parent and child records causing a
    stack level too deep, this fix prevents this by checking for the
    presence of the record in the output_hash first, added inverse of to
    test models as well to test for this condition.

--- spec/dummy_environments/sqlite/dummy1/models/post.rb
+++ spec/dummy_environments/sqlite/dummy1/models/post.rb
@@ -1,7 +1,7 @@
 require_relative './serialized_attribute_encoder'
 class Post < ActiveRecord::Base
   belongs_to :author
-  has_many :comments
+  has_many :comments, :inverse_of => :post
   attr_accessible :content, :name, :serialized_thing
   serialize :serialized_thing, SerializedAttributeEncoder.new
 end
--- test/models/comment.rb
+++ test/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :author
-  belongs_to :post
+  belongs_to :post, :inverse_of => :comments
   attr_accessible :content
 end
