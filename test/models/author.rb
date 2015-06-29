9e6728e3eb47950e2158c3e159036173a36eeea0
Ian Whitney <whit0694@umn.edu>
2015-06-29 08:22:45 -0500

    Remove unused test directory
    Closes #14
    
    These tests were ported to Rspec so the now-unused tests can be removed.

--- test/models/author.rb
+++
@@ -1,5 +0,0 @@
-class Author < ActiveRecord::Base
-  attr_accessible :name
-  has_many :posts
-  has_many :comments
-end
--- test/models/group_member.rb
+++
@@ -1,5 +0,0 @@
-class GroupMember < ActiveRecord::Base
-  belongs_to :author
-  belongs_to :group
-  attr_accessible :role
-end
